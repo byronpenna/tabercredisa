656d38b76bfd797bcf5d63d5d7fb245565fa9213
byronpenna <byronpenna@gmail.com>
2013-12-14 12:08:44 -0600

    start new day 14-12-13
    All Design

--- site_media/js/index/script.js
+++ site_media/js/index/script.js
@@ -8,4 +8,11 @@ $(document).ready(function(){
 	      $('body').removeClass('loading');
 	    }
 	});
+	$("#pluginFacebook iframe").each(function(){
+		alert('hola');
+	});
+	// var x = document.getElementsByTagName("f3b3eef3c4");
+	// x.style.with = '100%';
+	// x.style.background = 'white';
+	// alert($(x).attr("name"););
 });
